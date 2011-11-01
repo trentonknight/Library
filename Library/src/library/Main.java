298cf7bd544a6c2cd36fc7689e5d429dbe38a96b
trentonknight <trentonknight@T100.Skynet>
2011-10-31 22:13:50 -0400

    second commit

--- Library/src/library/Main.java
+++ Library/src/library/Main.java
@@ -15,24 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-         // Create two different Bicycle objects
-          Book bike1 = new Book();
-          Book bike2 = new Book();
-
-          // Invoke methods on those objects
-          bike1.changeCadence(50);
-          bike1.speedUp(10);
-          bike1.changeGear(2);
-          bike1.printStates();
-
-          bike2.changeCadence(50);
-          bike2.speedUp(10);
-          bike2.changeGear(2);
-          bike2.changeCadence(40);
-          bike2.speedUp(10);
-          bike2.changeGear(3);
-          bike2.printStates();
+       
     }
 
 }
