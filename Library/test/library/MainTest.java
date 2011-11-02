5d89e89295d106e54962752b1d0be822e401a6e2
Jason Mansfield <trentonknight@gmail.com>
2011-11-02 18:35:22 -0400

    JUnit for main as well.

---
+++ Library/test/library/MainTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author trentonknight
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+    
+    /**
+     * Test of main method, of class Main.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        Main.main(args);
+       
+    }
+}
